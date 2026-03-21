e716bfe1685e2422a304ffdb8aa46b411eac5926
Sami Shukri <samiziiv@gmail.com>
2026-03-20 22:36:36 -0600

    fix(a11y): add aria-label to ResizableHandle for accessible name [TASK-020]

--- src/components/Resizable/Resizable.tsx
+++ src/components/Resizable/Resizable.tsx
@@ -32,12 +32,14 @@ ResizablePanel.displayName = "ResizablePanel";
 
 interface ResizableHandleProps extends SeparatorProps {
   withHandle?: boolean;
+  "aria-label"?: string;
 }
 
-function ResizableHandle({ withHandle, className, ref, ...props }: ResizableHandleProps & { ref?: React.Ref<HTMLDivElement> }) {
+function ResizableHandle({ withHandle, className, ref, "aria-label": ariaLabel = "Resize panel", ...props }: ResizableHandleProps & { ref?: React.Ref<HTMLDivElement> }) {
   return (
     <Separator
       elementRef={ref}
+      aria-label={ariaLabel}
       className={cn(
         "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[orientation=vertical]:h-px data-[orientation=vertical]:w-full data-[orientation=vertical]:after:left-0 data-[orientation=vertical]:after:h-1 data-[orientation=vertical]:after:w-full data-[orientation=vertical]:after:-translate-y-1/2 data-[orientation=vertical]:after:translate-x-0",
         className
