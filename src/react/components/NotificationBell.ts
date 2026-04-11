e3132ebcf4f8d22c57a3aeb2e09349a431ee3413
Sami Shukri <samiziiv@gmail.com>
2026-04-11 06:10:18 -0600

    feat(react): add react adapters for all 108 base components

---
+++ src/react/components/NotificationBell.ts
@@ -0,0 +1,6 @@
+export { NotificationBell as LaNotificationBell } from "../../components/NotificationBell";
+export { notificationBellVariants as laNotificationBellVariants } from "../../components/NotificationBell";
+export type { NotificationBellProps as LaNotificationBellProps } from "../../components/NotificationBell";
+export type { NotificationItem as LaNotificationItem } from "../../components/NotificationBell";
+export type { NotificationBellVariants as LaNotificationBellVariants } from "../../components/NotificationBell";
+export type { BadgeVariants as LaBadgeVariants } from "../../components/NotificationBell";
---
+++ src/react/components/Sonner.ts
@@ -0,0 +1,3 @@
+export { Toaster as LaToaster } from "../../components/Sonner";
+export type { ToasterProps as LaToasterProps } from "../../components/Sonner";
+export { toast as laToast } from "../../components/Sonner";
